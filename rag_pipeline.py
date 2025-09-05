4ad762f0fddb3bd8d70465c1bc998c8164e5f132
Aditya-gautam21 <153100646+Aditya-gautam21@users.noreply.github.com>
2025-09-05 11:52:59 +0530

    update

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -1,2 +1,11 @@
 import os, json, uuid, typing as t
 from fastapi import FastAPI, HTTPException, APIRouter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import chroma
+from langchain.chains import RetrievalQA
+from langchain.llms import HuggingFaceHub
+
+DATA_PATH = "data"
+DB_PATH = "db"
+
