ae26c2bd986714b6f5603f349d7b46f7d39c5275
Aditya-gautam21 <153100646+Aditya-gautam21@users.noreply.github.com>
2025-09-29 22:16:30 +0530

    update

--- app.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-import torch
-import streamlit as st
-from dotenv import load_dotenv
-##fe fi foe fum
-# du du tu du mv
-load_dotenv()
-google_api_key = os.getenv("GOOGLE_API_KEY")
